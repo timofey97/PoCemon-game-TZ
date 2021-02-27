0aa1cf00c641e11608aeec5c0a1613cdf3f6da05
Timofey <gleb285@gail.com>
2021-02-27 20:03:35 +0100

    X

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Modal from '../Modal';
 import Menu from './Menu';
 import NavBar from './navBar';
 
@@ -6,6 +7,7 @@ import NavBar from './navBar';
 
 const MenuHeader = ({bgActive}) => {
     const [isActive, setActive] =  useState(null);
+    const [isOpenModal, setOpenModal] =  useState(false);
     
     const handClickHamburg = () => {
         setActive(prevState => !prevState)
@@ -13,6 +15,10 @@ const MenuHeader = ({bgActive}) => {
 
     const onMenuClick = () => {
         setActive(false);
+    }
+
+    const handlerClickLogin = () => {
+        setOpenModal(prevState => !prevState);
     }
     
     return (
@@ -24,8 +30,16 @@ const MenuHeader = ({bgActive}) => {
             <NavBar 
                 isActive={isActive} 
                 handClickHamburg={handClickHamburg} 
-                bgActive={bgActive}/>
-            
+                bgActive={bgActive}
+                onClickLogin={handlerClickLogin}/>
+        
+               
+            <Modal 
+                    isOpen={isOpenModal}
+                    title="Log in..."
+                    onCloseModal={handlerClickLogin}>
+                Some text...
+            </Modal>
         </>
     )
 }
